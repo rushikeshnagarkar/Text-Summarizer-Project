8c83ee3b804c6d6005be3d878683239194c45283
rushikeshnagarkar <138763048+rushikeshnagarkar@users.noreply.github.com>
2024-01-10 19:02:12 -1200

    Create model_evaluation.py

---
+++ src/textSummarizer/conponents/model_evaluation.py
@@ -0,0 +1,21 @@
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from datasets import load_dataset, load_from_disk, load_metric
+import torch
+import pandas as pd
+from tqdm import tqdm
+from textSummarizer.entity import ModelEvaluationConfig
+
+
+
+
+class ModelEvaluation:
+    def __init__(self, config: ModelEvaluationConfig):
+        self.config = config
+
+
+    
+    def generate_batch_sized_chunks(self,list_of_elements, batch_size):
+        """split the dataset into smaller batches that we can process simultaneously
+        Yield successive batch-sized chunks from list_of_elements."""
+        for i in range(0, len(list_of_elements), batch_size):
+            yield list_of_elements[i : i + batch_size]
